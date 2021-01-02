4ba9b3961220df5e77ec2da23ec55f3a01af8102
Connor Youngs <connormyoungs@gmail.com>
2021-01-02 18:13:11 +0000

    Added the -u flag to python in index.js to force flushing to stdout

--- lab/script_running/child_process_spawn_test/index.js
+++ lab/script_running/child_process_spawn_test/index.js
@@ -8,18 +8,18 @@ app.get('/', (req, res) => {
  
  var dataToSend;
  // spawn new child process to call the python script
- const python = spawn('python', ['script1.py']);
+ const python = spawn('python', ['-u', 'script1.py']);
  // collect data from script
  python.stdout.on('data', function (data) {
   dataToSend = data.toString();
   console.log('Pipe data from python script ...');
   console.log(dataToSend);
-   res.send(dataToSend)
  });
  // in close event we are sure that stream from child process is closed
  python.on('close', (code) => {
  console.log(`child process close all stdio with code ${code}`);
  // send data to browser
+res.send(dataToSend);
  });
  
 })
